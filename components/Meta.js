b31916579cb701f4808d2bfc7308c567e187d549
sheadscott <sheadscott@gmail.com>
2019-03-21 07:31:05 -0500

    Fixed FOUT issue

--- components/Meta.js
+++ components/Meta.js
@@ -2,6 +2,10 @@ import Head from "next/head";
 
 const Meta = () => (
   <Head>
+    <link
+      href="https://fonts.googleapis.com/css?family=Bungee+Shade|Roboto"
+      rel="stylesheet"
+    />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <title>Next JS Template</title>
