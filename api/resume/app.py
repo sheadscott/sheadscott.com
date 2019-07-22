6e4288699ec0f84d9b4996d85b076417c71d26ba
sheadscott <sheadscott@gmail.com>
2019-07-22 14:45:04 -0500

    Modified api to work on Now

--- api/resume/app.py
+++ api/resume/app.py
@@ -1,50 +1,13 @@
-from flask import Flask
+from os import environ
+from flask import Flask, Response
 from pathlib import Path
 app = Flask(__name__)
 
-@app.route('/api/resume/basics/')
-def basics():
-    basics_data = Path('json/basics.json').read_text()
-    return basics_data 
-
-@app.route('/api/resume/summary/')
-def summary():
-    summary_data = Path('json/summary.json').read_text()
-    return summary_data 
-
-@app.route('/api/resume/education/')
-def education():
-    education_data = Path('json/education.json').read_text()
-    return education_data
-
-@app.route('/api/resume/work/')
-def work():
-    work_data = Path('json/work.json').read_text()
-    return work_data
-
-@app.route('/api/resume/training/')
-def training():
-    training_data = Path('json/training.json').read_text()
-    return training_data
-
-@app.route('/api/resume/skills/')
-def skills():
-    skills_data = Path('json/skills.json').read_text()
-    return skills_data
-
-@app.route('/api/resume/projects/')
-def projects():
-    return """{
-
-    }"""
-
-@app.route('/api/resume/volunteer/')
-def volunteer():
-    return """{
-
-    }"""
-
-app.run(port=5000)
+@app.route('/api/resume/<path:path>/')
+def catch_all(path):
+    data = Path(f'json/{path}.json').read_text()
+    return Response(data, mimetype="application/json")
 
 
-
+if environ.get('ENV') == 'dev':
+    app.run(port=5000)
